98598e1af80e8523cda90273be3f4f07291b7aba
Usuario40028922 <165251305+Usuario40028922@users.noreply.github.com>
2025-04-11 09:36:39 -0300

    VAI QUE É TUA TAFAREEEEELLLLL

---
+++ flask-tutorial/flaskr/auth.py
@@ -0,0 +1,99 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif username == "WTF" and password == "horse!":
+            return redirect(url_for("easteregg"))
+
+        if error is None:
+            
+            try:
+                with open("flaskr/jogadores.txt","w") as arquivo:
+                    arquivo.write(f"{username},{password},")
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, password),
+                )
+                db.commit()
+            
+
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif user['password'] != password:  # Comparação direta da senha
+            error = 'Incorrect password.'
+
+        if error is None:
+            
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('Tudoprontopodejogar'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.register'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
